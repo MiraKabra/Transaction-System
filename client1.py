cde4054df0c616f4a279a919d493f24e6601d536
Gabrielle Lake <gabrielle.k.lake@gmail.com>
2023-12-14 00:45:06 -0800

    added metrics for evaluation

--- client1.py
+++ client1.py
@@ -7,7 +7,8 @@ import time
 import numpy as np
 import sys
 # Prompts for user arguments for a given transaction one by one
-
+global total_latency
+total_latency = 0
 NUM_USERS = len(core.user_map.keys())
 
 def two_hop_operation(transaction_num, first_hop_param, second_hop_param):
@@ -90,11 +91,13 @@ def util(index, transaction_argument_map, isFirstHop, transaction_num = 0):
 @click.option("--price", prompt="Enter the book price", type=int)
 @click.option("--isbn", prompt="Enter the book's ISBN", type=str)
 def t1(title, fn, ln, price, isbn):
+    global total_latency
     ts = time.time()
     first_hop_param = [1, ts, title, fn, ln, price, isbn]
     second_hop_param = [2, ts, title, fn, ln, price, isbn]
 
     latency = two_hop_operation(1, first_hop_param, second_hop_param)
+    total_latency += latency
     return latency
 
 # Transaction 2: Update book price
@@ -102,10 +105,12 @@ def t1(title, fn, ln, price, isbn):
 @click.option("--title", prompt="Enter the book title", type=str)
 @click.option("--price", prompt="Enter the book price", type=int)
 def t2(title, price):
+    global total_latency
     ts = time.time()
     first_hop_param = [1, ts, title, price]
     second_hop_param = [2, ts, title, price]
     latency = two_hop_operation(2, first_hop_param, second_hop_param)
+    total_latency += latency
     return latency
 
 # Transaction 3: Retrieve author information
@@ -113,9 +118,11 @@ def t2(title, price):
 @click.option("--fn", prompt="Enter the author's first name", type=str)
 @click.option("--ln", prompt="Enter the author's last name", type=str)
 def t3(fn, ln):
+    global total_latency
     ts = time.time()
     first_hop_param = [1, ts, fn, ln]
-    latency = one_hop_operation(3, first_hop_param)
+    param, latency = one_hop_operation(3, first_hop_param)
+    total_latency += latency
     return latency
 
 # Transaction 4: Update author description
@@ -124,9 +131,11 @@ def t3(fn, ln):
 @click.option("--ln", prompt="Enter the author's last name", type=str)
 @click.option("--desc", prompt="Enter the author description", type=str)
 def t4(fn, ln, desc):
+    global total_latency
     ts = time.time()
     first_hop_param = [1, ts, fn, ln, desc]
-    latency = one_hop_operation(4, first_hop_param)
+    param, latency = one_hop_operation(4, first_hop_param)
+    total_latency += latency
     return latency
 
 # Transaction 5: Record a sale
@@ -134,19 +143,23 @@ def t4(fn, ln, desc):
 @click.option("--title", prompt="Enter the book title", type=str)
 @click.option("--quantity", prompt="Enter the quantity of books purchased", type=int)
 def t5(title, quantity):
+    global total_latency
     ts = time.time()
     first_hop_param = [1, ts, title, quantity]
     second_hop_param = [2, ts, title, quantity]
     latency = two_hop_operation(5, first_hop_param, second_hop_param)
+    total_latency += latency
     return latency
 
 # Transaction 6: Remove a sale record
 @click.command()
 @click.option("--saleid", prompt="Enter the sale record ID", type=str)
 def t6(saleid):
+    global total_latency
     ts = time.time()
     first_hop_param = [1, ts, saleid]
-    latency = one_hop_operation(6, first_hop_param)
+    param, latency = one_hop_operation(6, first_hop_param)
+    total_latency += latency
     return latency
 
 def getUserInput(inputMethod=0, path=None):
@@ -175,6 +188,8 @@ def getUserInput(inputMethod=0, path=None):
             return mode
     elif inputMethod==1:
         file = open(path, 'r')
+        txnCount = 0
+        startTime = time.time()
         while True:
             # read file line by line
             line = file.readline()
@@ -182,26 +197,37 @@ def getUserInput(inputMethod=0, path=None):
             mode = int(line[0])
             if mode == 1:
                 inp = re.split('(--title|--fn|--ln|--price|--isbn)', line[:-1])[1:]
+                txnCount += 1
             elif mode == 2:
                 inp = re.split('(--title|--price)', line[:-1])[1:]
+                txnCount += 1
             elif mode == 3:
                 inp = re.split('(--fn|--ln)', line[:-1])[1:]
+                txnCount += 1
             elif mode == 4:
                 inp = re.split('(--fn|--ln|--desc)', line[:-1])[1:]
+                txnCount += 1
             elif mode == 5:
                 inp = re.split('(--title|--quantity)', line[:-1])[1:]
+                txnCount += 1
             elif mode == 6:
                 inp = re.split('(--saleid)', line[:-1])[1:]
+                txnCount += 1
             elif mode == 7:
-                return mode
+                break
             inp = [i.strip() for i in inp]
             options[mode](inp, standalone_mode=False)
-            time.sleep(2)
+        endTime = time.time()
         file.close()
+    if total_latency > 0:
+        print("Total Duration:", endTime - startTime)
+        print("Total # of Txns:", txnCount)
+        print("Throughput:", (txnCount / float(total_latency)))
+    return mode
 
 
 if __name__ == '__main__':
-    inputMethod = 0
+    inputMethod = 1
     if inputMethod == 1:
         path = sys.argv[1]
         data_file = open("dataFile.txt", "a")
